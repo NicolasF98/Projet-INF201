9370a482a8c9563b5a4697d2d9094f1232d6ab17
Nicolas F <47786367+NicolasF98@users.noreply.github.com>
2020-03-11 15:27:33 +0100

    Create README.ml

---
+++ README.ml
@@ -0,0 +1,58 @@
+# INF201: Projet de fin de semestre - Le jeu du Rummikub
+Université de Grenoble, Licence Sciences & Technologies, 1ère année semestre 2.
+
+UE INF201: Algorithmique et Programmation fonctionnelle.
+
+2019 - 2020
+
+---
+
+## Info
+- Nicolas Fournout <nicolas.fournout@etu.univ-grenoble-alpes.fr>
+
+## Deadlines
+
+**Dimanche 22 Mars 2020 à 23h59**
+ - [ ] Q1 (partie 3) et Q2 (partie 4).
+
+**Veille avant la soutenance**
+ - [ ] Toutes les autres questions.
+
+### Partie 3: multi-ensembles
+
+- [x] 1 - Créations des fonctions opérant sur les multi-ensembles.
+- [x] 2 - Implémentation des multi-ensembles sur un type somme.
+
+### Partie 4: graphisme avec Turtle
+
+- [ ] 1 - Réusinage de la partie 3.
+
+### Partie 5: le Rummikub
+
+- [ ] 1 - Création de la table de jeu.
+- [ ] 2 - Distribution initiale.
+- [ ] 3 - Tour de jeu.
+- [ ] 3 - Combinaison vialide.
+- [ ] 3 - Première pose.
+- [ ] 4 - Arrêt du jeu.
+- [ ] 5 - Calcul du score et joueur gagnant.
+  
+### Partie 6: Modélisation ds données
+
+- [ ] 1 - Les tuiles.
+- [ ] 2 - Combinaisons, tables et poses.
+- [ ] 3 - Les joueurs.
+- [ ] 4 - Etat d'une partie.
+
+### Partie 7: Mise en oeuvre des règes 
+
+- [ ] 1 - Validité des combinaisons.
+- [ ] 2 - Calcul de points
+
+### Partie 8: Gameplay
+
+- [ ] 1 - Validation des coups.
+- [ ] 2 - Ajout d'une tuile sur la table.
+- [ ] 3 - Recherche de combinaisons dans une main.
+- [ ] 4 - Changement d'état.
+- [ ] 5 - Scenario de jeu.
